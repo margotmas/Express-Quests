dc2d500fb997ab3af0c77a8dd6c2413be8d8174d
Margot <margot.voydis@gmail.com>
2024-02-13 11:15:46 +0100

    finished challenge express 02.1

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -42,7 +42,6 @@ const postMovie = (req, res) => {
       res.status(201).send({id: result.insertId});
     })
     .catch((err) => {
-      console.error(err);
       res.sendStatus(500);
     });
 };
